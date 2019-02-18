75f488bdd0ca843bb4bb19de2dfe5f6ce8061652
Fatai Balogun <fattylee.remod@gmail.com>
2019-02-18 22:10:12 +0100

    split-up primeNumbers files into modules

--- isPrime.js
+++ isPrime.js
@@ -11,24 +11,4 @@ const isPrime = (number) => {
   return true;
 };
 
-const primeNumbers = (min, max) => {
-  if(max === undefined){
-    max = min;
-    min = 1;
-  }
-
-  if (typeof min !== 'number' || (typeof max !== 'number') ) {
-    throw TypeError('Only number argument is allowed');
-  }
-  
-  const numbers = [];
-  for(; min <= max; min++){
-    if(isPrime(min)) {
-      numbers.push(min);
-    }
-  }
-  return numbers.length ? numbers: [];
-};
-
-console.log(primeNumbers(78,100).join(','));
-console.log(primeNumbers(7).join(','));
+module.exports = isPrime;
